67234aae82fc5e0a51d724306e115dca56057206
bwg5079 <bwg5079@44a738fa-cea4-4c8e-a93f-a88c45f461a7>
2012-09-04 22:00:45 +0000

    add extra output on program behavior
    git-svn-id: https://subversion.assembla.com/svn/bgolden_cmpsc@2 44a738fa-cea4-4c8e-a93f-a88c45f461a7

--- cmpsc465/hw1/ctests.c
+++ cmpsc465/hw1/ctests.c
@@ -3,18 +3,21 @@
 
 int main() 
 {
-	int i, j; 
+	int i, j, k; 
 	size_t size;
 	int A[] = {0, 1, 2, 4, 17, 12, 6, 9, 10, 11, 22, 
 		   23, 24, 8, 5, 13, 14, 15, 16, 18, 19};
 	
-	size = sizeof(A) / sizeof(int);
-
-	printf("size = %zu\n", size);
-
 	i = 1;
 	j = 2;
+	size = sizeof(A) / sizeof(int);	
 
+	printf("test array = {");
+	for (k = 0; k < (int) (size - 1); k++)
+		printf("%d, ", A[k]);
+	printf("%d}\n", A[k]);
+
+	printf("i = %d, j = %d\n", i, j);
 	printf("(A + i)[j] = %d\n", (A + i)[j]);
 	printf("A[i + j] = %d\n", A[i + j]);
 
