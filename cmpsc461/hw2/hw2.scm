dfd459d101923cfd8089a62816fb37f890f92907
bwg5079 <bwg5079@44a738fa-cea4-4c8e-a93f-a88c45f461a7>
2013-01-25 01:32:53 +0000

    replace 'removeFirst' and 'removeLast' functions with 'remove-first-n' and 'remove-last-n'
    git-svn-id: https://subversion.assembla.com/svn/bgolden_cmpsc@14 44a738fa-cea4-4c8e-a93f-a88c45f461a7

--- cmpsc461/hw2/hw2.scm
+++ cmpsc461/hw2/hw2.scm
@@ -10,21 +10,9 @@
   (let ((len (length xs)))
     (cond ((or (null? xs) (<= b a)) '())
 	  ((> a len) xs)  ;; a > len, return eveything
-	  ((> b len) (removeFirst xs a 0))  ;; b > len, return everything before a
-	  (#t (removeFirst (removeLast xs (- b a 1)) a 0)))))
+	  ((> b len) (remove-first-n xs a))  ;; b > len, return everything before a
+	  (#t (remove-first-n (remove-last-n xs (- b a 1)) a)))))
 
-;;; remove the last n elements of xs
-(define (removeLast xs n)
-  (if (null? xs) 
-      xs
-      (reverse (removeFirst (reverse xs) n 0))))
-
-;;; remove the first elements of xs, from n to a
-;;; should be called as (removeFirst xs a 0) to remove the elements before a
-(define (removeFirst xs a n)
-  (cond ((null? xs) xs)
-	((< n a) (removeFirst (cdr xs) a (+ n 1)))
-	(#t xs)))
 
 ;;; cartProduct(list1, list2) is the Cartesian Product of list1 and list2, 
 ;;;   i.e. list1 x list2
@@ -32,14 +20,6 @@
    (if (or (null? list1) (null? list2)) 
        '()
        (append (makePairs (car list1) list2) (cartProduct (cdr list1) list2))))
-       
-;;; return a list of all pairs where the first element is a 
-;;;   and the second element is an element of xs
-;;; e.g. a = 1, xs = '(2 3 4), return '((1 2) (1 3) (1 4))
-(define (makePairs a xs)
-  (if (null? xs) 
-      xs
-      (cons (list a (car xs)) (makePairs a (cdr xs)))))
 
 ;;; compress(xs) returns a compressed version of xs such that:
 ;;;   for each contiguous region of two or more equal elements in the list, 
@@ -51,13 +31,7 @@
 		  (count (countAdj xs first)))
 	      (if (= count 1) 
 		  (cons first (compress (cdr xs)))
-		  (cons (list count first) (compress (removeFirst xs count 0))))))))
-
-;;; count the number of elements adjacent to the head of the list
-(define (countAdj xs e)
-  (cond ((null? xs) 0)
-	((equal? (car xs) e) (+ (countAdj (cdr xs) e) 1))
-	(#t 0)))
+		  (cons (list count first) (compress (remove-first-n xs count))))))))
 
 ;;; expand(clist) returns an expanded version of a compressed list, 
 ;;;   as constructed using the compress function above
@@ -66,12 +40,6 @@
 	((list? (car clist)) (append (duplicate (caar clist) (cadr (car clist))) (expand (cdr clist))))
 	(#t (cons (car clist) (expand (cdr clist))))))  ;; head of the list is a single element
 
-;;; return a list with n elements, all elements are x
-(define (duplicate n x)
-  (if (zero? n)
-      '()
-      (cons x (duplicate (- n 1) x))))
-
 ;;; interleaves the first half of xs and the reverse of the second half of xs
 ;;; (e.g. list = '(1 8 4 9 5 2 3 7 6) --> output = '(1 6 8 7 4 3 9 2 5))
 (define (mangle xs)
@@ -79,11 +47,52 @@
       xs
       (let* ((len (length xs))
 	     (m (ceiling (/ len 2)))
-	     (first (removeLast xs (- m 1)))		;; first half of xs
-	     (rest (reverse (removeFirst xs m 0))))	;; reverse of second half of xs
+	     (first (remove-last-n xs (- m 1)))		;; first half of xs
+	     (rest (reverse (remove-first-n xs m))))	;; reverse of second half of xs
 	(if (odd? len)
 	    (interleave first rest)   			   ;; interleave the two parts
 	    (interleave first (remove-last-elem rest)))))) ;; remove last element of rest, then interleave
+
+;;;;;;; Auxiliary functions
+
+;;; remove the last element of xs
+(define (remove-last-elem xs)
+  (if (null? xs) 
+      xs
+      (reverse (cdr (reverse xs)))))
+
+;;; remove the last n elements of xs
+(define (remove-last-n xs n) 
+  (if (or (null? xs) (zero? n))
+      xs
+      (remove-last-n (remove-last-elem xs) (- n 1))))
+
+;;; remove the first n elements of xs
+(define (remove-first-n xs n)
+  (if (or (null? xs) (zero? n))
+      xs
+      (remove-first-n (cdr xs) (- n 1))))
+
+;;; return a list of all pairs where the first element is a 
+;;;   and the second element is an element of xs
+;;; e.g. a = 1, xs = '(2 3 4), return '((1 2) (1 3) (1 4))
+(define (makePairs a xs)
+  (if (null? xs) 
+      xs
+      (cons (list a (car xs)) (makePairs a (cdr xs)))))
+
+;;; return a list with n elements, all elements are x
+(define (duplicate n x)
+  (if (zero? n)
+      '()
+      (cons x (duplicate (- n 1) x))))
+
+;;; count the number of elements adjacent to the 
+;;;   head of the list which are the same
+(define (countAdj xs e)
+  (cond ((null? xs) 0)
+	((equal? (car xs) e) (+ (countAdj (cdr xs) e) 1))
+	(#t 0)))
 
 ;;; combine xs and ys by first taking the head of xs and then head of ys 
 ;;;   on each iteration
@@ -94,7 +103,3 @@
 	((null? ys) xs)
 	(#t (append (list (car xs) (car ys)) (interleave (cdr xs) (cdr ys))))))
 
-(define (remove-last-elem xs)
-  (if (null? xs) 
-      xs
-      (reverse (cdr (reverse xs)))))
