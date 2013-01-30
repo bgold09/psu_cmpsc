d71cfd36dc2ce0689c2c48375251eb44244ac8b2
bwg5079 <bwg5079@44a738fa-cea4-4c8e-a93f-a88c45f461a7>
2013-01-30 21:32:55 +0000

    working solution to p2 (merge-streams); added aux functions
    git-svn-id: https://subversion.assembla.com/svn/bgolden_cmpsc@17 44a738fa-cea4-4c8e-a93f-a88c45f461a7

--- cmpsc461/hw3/hw3.scm
+++ cmpsc461/hw3/hw3.scm
@@ -3,4 +3,36 @@
 ;;; Homework 3
 ;;; 2/2/13
 
+;;; difference(A, B)
 
+
+
+;;; merge-streams takes two ordered streams of integers and 
+;;;   merges them into one ordered stream
+;;; example: s1 = (1 3 4 8 8 10 ... ), s2 = (0 1 2 9 12 13 30)
+;;;   result: (0 1 1 2 3 4 8 8 9 10 12 13 23 30 ... )
+(define (merge-streams s1 s2) 
+  (cond ((and (empty-stream? s1) (empty-stream? s2)) the-empty-stream)
+	((empty-stream? s1) s2)
+	((empty-stream? s2) s1)
+	(#t (cons (head s1) (delay (cons (head s2) (delay (merge-streams (tail s1) (tail s2)))))))))
+
+
+;;; auxiliary functions
+
+(define head car)
+(define (tail stream) (force (cdr stream)))
+(define empty-stream? null?)
+(define the-empty-stream '())
+
+(define ones (cons 1 (delay ones)))
+
+(define (stream-section n stream)
+  (if (= n 0)
+      '()
+      (cons (head stream) (stream-section (- n 1) (tail stream)))))
+
+(define (integers-starting-from n)
+  (cons n (delay (integers-starting-from (+ n 1)))))
+
+(define Nats (integers-starting-from 0))
