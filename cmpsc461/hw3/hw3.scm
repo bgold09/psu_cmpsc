021948df7330bead5848988060f849be66f507c4
bwg5079 <bwg5079@44a738fa-cea4-4c8e-a93f-a88c45f461a7>
2013-02-02 17:06:48 +0000

    working solution to p3 (orderedTriples)
    git-svn-id: https://subversion.assembla.com/svn/bgolden_cmpsc@18 44a738fa-cea4-4c8e-a93f-a88c45f461a7

--- cmpsc461/hw3/hw3.scm
+++ cmpsc461/hw3/hw3.scm
@@ -17,8 +17,56 @@
 	((empty-stream? s2) s1)
 	(#t (cons (head s1) (delay (cons (head s2) (delay (merge-streams (tail s1) (tail s2)))))))))
 
+;;; orderedTriples returns all ordered triples (i, j, k) such that 
+;;;   0 <= i <= j <= k <= n, where n >= 0
+(define (orderedTriples n) 
+  (if (< n 0)
+      '()
+      (append (cart-triples (reverse (stream-section (+ n 1) Nats)) n) (orderedTriples (- n 1)))))
+
+;;; cart-triples takes a list ys of elements <= k and returns all triples (i, j, k)
+;;;   with all i's <= current element in ys, j = head of ys, k = k
+(define (cart-triples ys k)
+  (if (or (null? ys) (< k 0))
+      '()
+      (let ((j (car ys)))
+	(append (makeTriples (stream-section (+ j 1) Nats) j k) (cart-triples (cdr ys) k)))))
+
+;;; makeTriples makes a list all possible triples containing an element of xs,
+;;;   followed by j, followed by k
+(define (makeTriples xs j k)
+  (if (null? xs)
+      xs
+      (cons (list (car xs) j k) (makeTriples (cdr xs) j k))))
+
+
+;;; caesar-cipher takes a list and an integer n and returns a function f 
+;;;   such that (f x) returns the element n elements to the right of x 
+;;;   in the original list.
+;;; If x is not in the original list, the function should return x
+
+
+;;; rot-n takes a list a and an integer and returns the list of elements of a
+;;;   replaced with the element dictated by the caesar-cipher defined for the 
+;;;   list of lower-case English letters and the given interger
+;;; example: (rot-n 'r 0 c k y ) 13) --> result: (e 0 p x 1)
+
+
 
 ;;; auxiliary functions
+
+(define (random-list n k)
+  (if (zero? n) '()
+      (cons (random k) (random-list (- n 1) k))))
+
+(define (filter p xs)
+  (if (null? xs) 
+      xs
+      (let ((rest (filter p (cdr xs)))
+	    (first (car xs)))
+	(if (p first)
+	    (cons first rest)
+	    rest))))
 
 (define head car)
 (define (tail stream) (force (cdr stream)))
