b62dbe8d659deaad4d4b6a5d12730ea5f999d343
bwg5079 <bwg5079@44a738fa-cea4-4c8e-a93f-a88c45f461a7>
2013-02-03 01:52:36 +0000

    working solutions to p1, p4 (difference, caesar-cipher)
    git-svn-id: https://subversion.assembla.com/svn/bgolden_cmpsc@20 44a738fa-cea4-4c8e-a93f-a88c45f461a7

--- cmpsc461/hw3/hw3.scm
+++ cmpsc461/hw3/hw3.scm
@@ -3,9 +3,10 @@
 ;;; Homework 3
 ;;; 2/2/13
 
-;;; difference(A, B)
-
-
+;;; difference(A, B) returns the list 
+;;;   A \ B = {x | (x is an element of A) ^ (x is not an element of B)} 
+(define (difference A B)
+  (filter (lambda (n) (not (fold-left (lambda (x y) (or x (equal? y n))) #f B))) A))
 
 ;;; merge-streams takes two ordered streams of integers and 
 ;;;   merges them into one ordered stream
@@ -39,30 +40,35 @@
       xs
       (cons (list (car xs) j k) (makeTriples (cdr xs) j k))))
 
-
 ;;; caesar-cipher takes a list and an integer n and returns a function f 
 ;;;   such that (f x) returns the element n elements to the right of x 
 ;;;   in the original list.
 ;;; If x is not in the original list, the function should return x
-;(define (caesar-cipher xs n)
-;  (lambda (n) ))
+(define (caesar-cipher xs n)
+  (lambda (x) 
+    (if (member? x xs)
+	(nth-element (modulo n (length xs)) (rotate xs (indexof x xs)))
+	x)))
 
-
-;;; rotate treats xs a circular list, such that the first element of the list 
-;;;   will come immediately after the last element. Retrieving the nth element
-;;;   of the list takes this into account, e.g. if the list is (a b c d), then 
-;;;   the 0th element is a, 1st is b, 4th is a, 5th is b, etc. 
+;;; rotate rotates the list xs n times such that after each rotation, 
+;;;   the first elemement of the new list is the current second element, 
+;;;   the second element is the current third, etc. and the last element 
+;;;   is the current first element
 (define (rotate xs n)
-  (cond ((null? xs) xs)
-	((= n 0) (car xs))
-	(#t (list-ref xs (modulo n (length xs))))))
+  (if (or (null? xs) (<= n 0))
+      xs
+      (rotate (append (cdr xs) (list (car xs))) (- n 1))))
 
 ;;; rot-n takes a list a and an integer and returns the list of elements of a
 ;;;   replaced with the element dictated by the caesar-cipher defined for the 
 ;;;   list of lower-case English letters and the given interger
 ;;; example: (rot-n 'r 0 c k y ) 13) --> result: (e 0 p x 1)
+(define (rot-n a n)
+  (if (null? a)
+      a
+      (cons ((caesar-cipher alph n) (car a)) (rot-n (cdr a) n))))
 
-
+(define alph '(a b c d e f g h i j k l m n o p q r s t u v w x y z))
 
 ;;; auxiliary functions
 
@@ -84,8 +90,6 @@
 (define empty-stream? null?)
 (define the-empty-stream '())
 
-(define ones (cons 1 (delay ones)))
-
 (define (stream-section n stream)
   (if (= n 0)
       '()
@@ -101,3 +105,14 @@
       #f
       (or (eq? x (car xs))
 	  (member? x (cdr xs)))))
+
+;;; returns the index of element matching a in xs, assumes a is in xs
+(define (indexof a xs)
+  (if (eq? (car xs) a)
+      0
+      (+ 1 (indexof a (cdr xs)))))
+
+(define (nth-element n xs)
+  (cond ((or (null? xs) (< n 0)) '())
+	((= n 0) (car xs))
+	(#t (nth-element (- n 1) (cdr xs)))))
