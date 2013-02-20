52d38109a508e089c2d28e0ca63605f0aa079722
bwg5079 <bwg5079@44a738fa-cea4-4c8e-a93f-a88c45f461a7>
2013-02-20 21:45:50 +0000

    working solution for p5 (simulate), access, update and delete ops; aux functions getbinding and update
    git-svn-id: https://subversion.assembla.com/svn/bgolden_cmpsc@28 44a738fa-cea4-4c8e-a93f-a88c45f461a7

---
+++ cmpsc461/hw5/hw5.scm
@@ -0,0 +1,34 @@
+;;; Brian Golden
+;;; CMPSC 461
+;;; Homework 5
+;;; 2/23/13
+
+;;; problem 5 - simulate
+
+;;; simulate(op, stack) takes a list of operations (op) and  a list of bindings
+;;;   (stack), each of which consists of a name (symbol) and a value
+;;; i.e. a binding (x . val), x is the variable and is bound to the value val
+;;; simulate simulates stack operations specified by the op arguments and 
+;;;   returns the appropriate values
+(define (simulate op stack)
+  (let ((fcn (car op))
+	(args (cdr op)))
+    (cond ((eq? fcn 'access) (cdr (getbinding (car args) stack)))
+	  ((eq? fcn 'update) (update (car args) (cadr args) stack))
+	  ((eq? fcn 'delete) (filter (lambda (x) (not (equal? (car x) (car args)))) stack))
+	  ((eq? fcn 'create) 3)
+	  (#t 6))))
+
+;;; getbinding returns the binding on the stack with the variable b
+(define (getbinding b stack)
+  (cond ((null? stack) '())
+	((equal? b (caar stack)) (car stack))
+	(#t (getbinding b (cdr stack)))))
+
+;;; update returns the stack with the binding matching x 
+;;;   with the new binding val
+(define (update x val stack)
+  (cond ((null? stack) '())
+	((equal? x (caar stack)) (append (list (cons x val)) (cdr stack)))
+	(#t (append (list (car stack)) (update x val (cdr stack))))))
+
