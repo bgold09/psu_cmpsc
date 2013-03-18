9269481f53a8b8e56aa1cb23a6e6a2cae930f1ca
brian.golden09@gmail.com <brian.golden09@gmail.com@44a738fa-cea4-4c8e-a93f-a88c45f461a7>
2013-03-18 21:19:08 +0000

    working solution to First-Fit allocation
    git-svn-id: https://subversion.assembla.com/svn/bgolden_cmpsc@54 44a738fa-cea4-4c8e-a93f-a88c45f461a7

--- cmpsc473/project2/my_memory.c
+++ cmpsc473/project2/my_memory.c
@@ -8,11 +8,133 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>  /* malloc(), free() */
+#include <stdlib.h>     /* malloc(), free() */
+#include "my_memory.h"
 
-int type;            /* memory allocation scheme to use */
+#define NUM_BUCKETS 11  /* number of block buckets needed for Buddy System */
+
+static int type;                      /* memory allocation scheme to use */
+free_list_t *free_list[NUM_BUCKETS];  /* free_lists for allocation */
 
 void *my_malloc(int size)
+{
+	block_t *block;
+	void *ret;
+
+	/* find block to use for allocation and remove it from the free list */
+	if ((block = find_and_remove_block(size + HEADER_SIZE)) == NULL) {
+		return NULL;
+	}
+	
+	ret = block->addr;
+	*((int *) ret) = size;
+
+	/* split the block, if necessary */
+	if (size + HEADER_SIZE == block->size) {
+		block_deallocate(block);
+	} else {
+		block->size -= size + HEADER_SIZE;
+		block->addr += size + HEADER_SIZE;
+		/* readd the block to the correct free_list */
+		free_list_enqueue(block);
+	}
+
+	return ret + HEADER_SIZE;
+}
+
+block_t *find_and_remove_block(int size)
+{
+	switch(type) {
+	case FF:
+		return find_and_remove_block_ff(size);
+		break;
+	case BF:
+		return find_and_remove_block_bf(size);
+		break;
+	case WF:
+		return find_and_remove_block_wf(size);
+		break;
+	case BS:
+		return find_and_remove_block_bs(size);
+		break;
+	default:
+		/* default to First Fit allocation */
+		return find_and_remove_block_ff(size);
+		break;
+	}
+}
+
+block_t *free_list_remove(free_list_t *list, block_t *block)
+{
+	block_t *p;
+
+	p = NULL;
+
+	/* one element list, assuming non-null */
+	if (list->head == list->tail) { 
+		p = list->head;
+		list->head = NULL;
+		list->tail = NULL;
+	} else if (list->head == block) {
+		p = list->head;
+		list->head = p->next;
+		list->head->prev = NULL;
+		p->next = NULL;
+		p->prev = NULL;
+	} else if (list->tail == block) {
+		p = list->tail;
+		list->tail = p->prev;
+		list->tail->next = NULL;
+		p->next = NULL;
+		p->prev = NULL;
+	} else {	
+		p = block;
+		p->prev->next = p->next;
+		p->next->prev = p->prev;
+		p->next = NULL;
+		p->prev = NULL;
+	}
+
+	return p;
+}
+
+block_t *find_and_remove_block_ff(int size)
+{
+	free_list_t *list;
+	block_t *p;
+
+	list = free_list[0];
+
+	if (list->head->size >= size) {
+		return free_list_remove(list, list->head);
+	} 
+
+	for (p = list->head->next; p != list->tail; p = p->next) {
+		if (p->size >= size) {
+			return free_list_remove(list, list->head);
+		}
+	}
+
+	if (list->tail->size >= size) {
+		return free_list_remove(list, list->tail);
+	}
+	
+	return NULL;
+}
+
+block_t *find_and_remove_block_bf(int size)
+{
+	(void) size;
+	return NULL;
+}
+
+block_t *find_and_remove_block_wf(int size)
+{
+	(void) size;
+	return NULL;
+}
+
+block_t *find_and_remove_block_bs(int size)
 {
 	(void) size;
 	return NULL;
@@ -20,12 +142,134 @@ void *my_malloc(int size)
 
 void my_free(void *ptr)
 {
-	(void) ptr;
+	int size;
+	block_t *block;
+	free_list_t *list;
+
+	size = *((int *) (ptr - HEADER_SIZE));
+	block = block_allocate(ptr - HEADER_SIZE, size + HEADER_SIZE);
+	list = free_list_enqueue(block);   /* add to correct free_list */
+	coalesce(list, block);
+}
+
+void coalesce(free_list_t *list, block_t *ptr)
+{
+	if (type != BS) {
+		if (ptr->next != NULL) {  /* not at tail of list */
+			if (ptr->addr + ptr->size == ptr->next->addr) {
+				ptr->size += ptr->next->size;
+				free_list_remove(list, ptr->next); 
+			}
+		} 
+
+		if (ptr->prev != NULL) {  /* not at head of the list */
+			if (ptr->prev->addr + ptr->prev->size == ptr->addr) {
+				ptr->prev->size += ptr->size;
+				free_list_remove(list, ptr);
+			}
+		}
+	} else {
+		return;
+	}
 }
 
 void setup(int malloc_type, int mem_size, void *start_of_memory)
 {
-	(void) malloc_type;
-	(void) mem_size;
-	(void) start_of_memory;
+	int i;
+
+	type = malloc_type;
+
+	if (malloc_type != BS) {
+		free_list[0] = free_list_allocate();
+		free_list[0]->head = block_allocate(start_of_memory, mem_size);
+		free_list[0]->tail = free_list[0]->head;
+	} else {
+		for (i = 0; i < NUM_BUCKETS; i++) {
+			free_list[i] = free_list_allocate();
+			/* determine where to put the initial free segment */
+		}
+	}
+}
+
+block_t *block_allocate(void *addr, int size)
+{
+	block_t *p;   /* pointer to new free block to allocate */
+
+	if ((p = malloc(sizeof(*p))) == NULL) {
+		fprintf(stderr, "malloc() failed\n");
+	} else {
+		p->addr = addr;
+		p->size = (size_t) size;
+		p->prev = NULL;
+		p->next = NULL;
+	}
+
+	return p;
+}
+
+void block_deallocate(block_t *p)
+{
+	free(p);
+	p = NULL;
+}
+
+free_list_t *free_list_allocate(void)
+{
+	free_list_t *p;
+
+	if ((p = (free_list_t *) malloc(sizeof(*p))) == NULL) {
+		fprintf(stderr, "malloc failed, free_list_allocate()\n");
+	}
+
+	return p;
+}
+
+
+free_list_t *free_list_enqueue(block_t *block)
+{
+	free_list_t *list;
+	block_t *p;
+
+	list = NULL;
+
+	if (type != BS) {
+		list = free_list[0];
+
+		if (list->head == NULL) {
+			block->next = NULL;
+			block->prev = NULL;
+			list->head = block;
+			list->tail = block;
+			return list;
+		}
+
+		if (list->head->addr > block->addr) {
+			p = list->head;
+			list->head = block;
+			list->head->next = p;
+			list->head->prev = NULL;
+			p->prev = list->head;
+			return list;
+		}
+
+		for (p = list->head->next; 
+		     p->addr < block->addr && p->next != NULL; p = p->next);
+
+		if (p->next != NULL) {  /* not enqueuing at the tail */
+			p->prev->next = block;
+			block->prev = p->prev;
+			block->next = p;
+			p->prev = block;
+		} else {                /* inserting after p, new tail */
+			p->next = block;
+			block->prev = p;
+			block->next = NULL;
+			list->tail = block;
+		}
+	} else {
+		return list;
+	}
+
+	return list;
+
 }
--- cmpsc473/project2/my_memory.h
+++ cmpsc473/project2/my_memory.h
@@ -6,10 +6,35 @@
  *
  */
 
-#define FF  0  /* First Fit */
-#define BF  1  /* Best Fit */
-#define WF  2  /* Worst Fit */
-#define BS  3  /* Buddy System */
+#define FF          0  /* First Fit */
+#define BF          1  /* Best Fit */
+#define WF          2  /* Worst Fit */
+#define BS          3  /* Buddy System */
+#define HEADER_SIZE 4  /* size of size header for an allocated block */
+
+/**
+ * struct _free_block - information about a free segment of heap memory
+ *
+ */
+struct _free_block
+{
+	void *addr;                /* starting address of this memory segment */
+	int size;                  /* size of this memory segment */
+	struct _free_block *next;  /* pointer to next block in free list */
+	struct _free_block *prev;  /* pointer to previous block in free list */
+};
+typedef struct _free_block block_t;
+
+/*
+ * struct _free_list - doubly-linked list of free blocks
+ *
+ */
+struct _free_list
+{
+	block_t *head;  /* head of the list, NULL indicates empty list */
+	block_t *tail;  /* tail of the list */
+};
+typedef struct _free_list free_list_t;
 
 /**
  * my_malloc - allocates memory segments from start_of_memory 
@@ -37,3 +62,104 @@ void my_free(void *ptr);
  * @return void
  */
 void setup(int malloc_type, int mem_size, void *start_of_memory);
+
+/**
+ * block_allocate - allocate a new block node for the free_list
+ *
+ * @param addr: starting address for the new free block
+ * @param size: size of the new free block
+ * @return pointer to allocated block_t
+ */
+block_t *block_allocate(void *addr, int size);
+
+/**
+ * block_deallocate - free the memory allocated to a node
+ *
+ * @param p: pointer to block info to deallocate
+ * @return void
+ */
+void block_deallocate(block_t *p);
+
+/**
+ * free_list_remove - remove a block from a free list
+ *
+ * @param list: list to remove from
+ * @param block: block to remove from the list
+ * @return pointer to the removed block
+ */
+block_t *free_list_remove(free_list_t *list, block_t *block);
+
+/**
+ * find_and_remove_block_ff - find a block on the free list using First Fit 
+ *                            allocation and remove it
+ *
+ * @param size: size of memory segment to allocate
+ * @return pointer to block to use for allocation on success, 
+ *         NULL no block large enough was found
+ */
+block_t *find_and_remove_block_ff(int size);
+
+/**
+ * find_and_remove_block_bf - find a block on the free list using Best Fit
+ *                            allocation and remove it
+ *
+ * @param size: size of memory segment to allocate
+ * @return pointer to block to use for allocation on success, 
+ *         NULL no block large enough was found
+ */
+block_t *find_and_remove_block_bf(int size);
+
+/**
+ * find_and_remove_block_wf - find a block on the free list using Worst Fit 
+ *                            allocation and remove it
+ *
+ * @param size: size of memory segment to allocate
+ * @return pointer to block to use for allocation on success, 
+ *         NULL no block large enough was found
+ */
+block_t *find_and_remove_block_wf(int size);
+
+/**
+ * find_and_remove_block_bs - find a block on the free list using Buddy System 
+ *                            allocation and remove it
+ *
+ * @param size: size of memory segment to allocate
+ * @return pointer to block to use for allocation on success, 
+ *         NULL no block large enough was found
+ */
+block_t *find_and_remove_block_bs(int size);
+
+/**
+ * find_and_remove_block - find a block on the free list to use for 
+ *                         allocation and remove it
+ *
+ * @param size: size of memory segment to allocate
+ * @return pointer to block to use for allocation on success, 
+ *         NULL no block large enough was found
+ */
+block_t *find_and_remove_block(int size);
+
+
+/**
+ * coalesce - merge free blocks adjacent to a newly freed block
+ *
+ * @param list: free list to coalesce in
+ * @param ptr: pointer to newly added free block to the free_list
+ * @return void
+ */
+void coalesce(free_list_t *list, block_t *ptr);
+
+/**
+ * free_list_allocate - allocate memory for a free list
+ * 
+ * @return pointer to new free_list
+ */
+free_list_t *free_list_allocate(void);
+
+/**
+ * free_list_enqueue - enqueue a new block to a free list
+ *
+ * @param block: new block to enqueue on the list
+ * @return pointer to free list the block was added to
+ */
+free_list_t *free_list_enqueue(block_t *block);
