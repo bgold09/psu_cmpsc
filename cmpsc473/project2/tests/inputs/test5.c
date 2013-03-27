23aa82204703a45f6e53b519f632f10f1290aad9
brian.golden09@gmail.com <brian.golden09@gmail.com@44a738fa-cea4-4c8e-a93f-a88c45f461a7>
2013-03-27 22:21:08 +0000

    fix input5 to match its output file
    git-svn-id: https://subversion.assembla.com/svn/bgolden_cmpsc@60 44a738fa-cea4-4c8e-a93f-a88c45f461a7

--- cmpsc473/project2/tests/inputs/test5.c
+++ cmpsc473/project2/tests/inputs/test5.c
@@ -18,7 +18,7 @@ int main()
     size=240*1024;
 	void* a=my_malloc(size);/*We have 4 bytes header to save the size of that chunk in memory so the output starts at 4*/
     if ((int)a==-1)
-        fprintf(pFile, "This size can not be allocated!");
+        fprintf(pFile, "This size can not be allocated!\n");
     else
     {
     	fprintf(pFile, "start of the chunk a: %f K\n",(float)((int)a-(int)RAM)/1024);
@@ -28,7 +28,7 @@ int main()
     size=100*1024;
     void* b=my_malloc(size);
     if ((int)b==-1)
-        fprintf(pFile, "This size can not be allocated!");
+        fprintf(pFile, "This size can not be allocated!\n");
     else
     {
     	fprintf(pFile, "start of the chunk b: %f K\n",(float)((int)b-(int)RAM)/1024);
@@ -38,40 +38,40 @@ int main()
     size=240*1024;
 	void* c=my_malloc(size);
     if ((int)c==-1)
-        fprintf(pFile, "This size can not be allocated!");
+        fprintf(pFile, "This size can not be allocated!\n");
     else
     {
-    	fprintf(pFile, "start of chunk c: %f K\n",(float)((int)c-(int)RAM)/1024);
+    	fprintf(pFile, "start of the chunk c: %f K\n",(float)((int)c-(int)RAM)/1024);
     	fprintf(pFile, "End of the chunk c: %f K\n\n",(float)((int)c+size-(int)RAM)/1024);
     }
 
     size=100*1024;
 	void* d=my_malloc(size);
     if ((int)d==-1)
-        fprintf(pFile, "This size can not be allocated!");
+        fprintf(pFile, "This size can not be allocated!\n");
     else
     {
-    	fprintf(pFile, "start of chunk d: %f K\n",(float)((int)d-(int)RAM)/1024);
+    	fprintf(pFile, "start of the chunk d: %f K\n",(float)((int)d-(int)RAM)/1024);
     	fprintf(pFile, "End of the chunk d: %f K\n\n",(float)((int)d+size-(int)RAM)/1024);
     }
 
     size=100*1024;
 	void* e=my_malloc(size);
     if ((int)e==-1)
-        fprintf(pFile, "This size can not be allocated!");
+        fprintf(pFile, "This size can not be allocated!\n");
     else
     {
-    	fprintf(pFile, "start of chunk e: %f K\n",(float)((int)e-(int)RAM)/1024);
+    	fprintf(pFile, "start of the chunk e: %f K\n",(float)((int)e-(int)RAM)/1024);
     	fprintf(pFile, "End of the chunk e: %f K\n\n",(float)((int)e+size-(int)RAM)/1024);
     }
 
     size=200*1024;
 	void* f=my_malloc(size);
     if ((int)f==-1)
-        fprintf(pFile, "This size can not be allocated!");
+        fprintf(pFile, "This size can not be allocated!\n");
     else
     {
-    	fprintf(pFile, "start of chunk f: %f K\n",(float)((int)f-(int)RAM)/1024);
+    	fprintf(pFile, "start of the chunk f: %f K\n",(float)((int)f-(int)RAM)/1024);
     	fprintf(pFile, "End of the chunk f: %f K\n\n",(float)((int)f+size-(int)RAM)/1024);
     }
     fclose (pFile);
