3a68513bc6959ddaa09b6f99d5e1bf62b1bc49f6
brian.golden09@gmail.com <brian.golden09@gmail.com@44a738fa-cea4-4c8e-a93f-a88c45f461a7>
2013-04-15 00:52:13 +0000

    add function documentation
    git-svn-id: https://subversion.assembla.com/svn/bgolden_cmpsc@86 44a738fa-cea4-4c8e-a93f-a88c45f461a7

--- cmpsc473/project3/473_mm.c
+++ cmpsc473/project3/473_mm.c
@@ -20,7 +20,6 @@ void mm_init(void* vm, int vm_size, int n_frames, int pagesize, int policy)
 	faults = 0;
 	write_backs = 0;
 	table_size = vm_size / pagesize;
-	offset_bits = (int) log2f((int) table_size);
 	max_frames = n_frames;
 	cur_frames = 0;
 	page_size = pagesize;
@@ -29,16 +28,17 @@ void mm_init(void* vm, int vm_size, int n_frames, int pagesize, int policy)
 	if (install_signal_handler(SIGSEGV, SIGSEGV_handler) == -1) {
 		fprintf(stderr, "install_signal_handler() failed: %s\n", 
 				strerror(errno));
-	} else if ((page_hist = (list_pages *) malloc(sizeof(*page_hist))) == NULL) {
+	} else if ((page_hist = (list_pages *) 
+			malloc(sizeof(*page_hist))) == NULL) {
 		fprintf(stderr, "malloc failed\n");
-	} else if ((page_table = (entry_t **) malloc(sizeof(*page_table) * table_size)) == NULL) {
+	} else if ((page_table = (entry_t **) 
+			malloc(sizeof(*page_table) * table_size)) == NULL) {
 		fprintf(stderr, "malloc failed\n");
 	} else if (mprotect(vm, vm_size, PROT_NONE) == -1) {
 		fprintf(stderr, "mprotect() failed: %s\n", strerror(errno));
 	} else {
 		for (i = 0; i < table_size; i++) {
 			page_table[i] = entry_allocate(i);
-			page_table[i]->index = i;
 		}
 
 		page_hist->head = NULL;
@@ -63,8 +63,10 @@ int install_signal_handler(int sig, sighandler_t func)
 	int ret;                  /* error indicator */
 
 	sigact.sa_flags = SA_SIGINFO;
-	sigemptyset(&sigact.sa_mask);         /* do not mask any interrupts while in handler */
-	sigact.sa_sigaction = func;           /* name of the signal handler function */
+
+	/* do not mask any interrupts while in handler */
+	sigemptyset(&sigact.sa_mask);         
+	sigact.sa_sigaction = func;           /* name of sig handler function */
 
 	ret = sigaction(sig, &sigact, NULL);  /* do the installation */
 	error = (ret == -1);                  /* error condition */
@@ -79,32 +81,19 @@ int install_signal_handler(int sig, sighandler_t func)
 
 void SIGSEGV_handler(int sig, siginfo_t *si, void *unused)
 {
-	int pagenum;
-	void *virt_page;
+	int pagenum;      /* page number of the virtual page being accessed */
+	void *virt_page;  /* address of the virtual page */
 
 	(void) unused;	
 
 	if (sig == SIGSEGV) {
-		/* printf("got SIGSEV at address 0x%lx\n", (long) si->si_addr); */
-		/* if page not present
-		 * 	bring it into physical memory
-		 * 	evict if necessary (num_frames == max_frames)
-		 * 		if modify bit set, increment write backs
-		 * 	increment faults
-		 * 	set read permissions
-		 * else 
-		 * 	set modify bit
-		 *	set read-write permissions
-		 */
-
 		pagenum = get_table_index(si->si_addr);
 		virt_page = get_page_base(pagenum);
 
-		if (!(page_table[pagenum]->flags & PB)) {  /* page not present */
-
+		if (!(page_table[pagenum]->flags & PB)) { /* page not present */
 			history_add(page_table[pagenum]);
 
-			if (cur_frames == max_frames) {  /* page table is full */
+			if (cur_frames == max_frames) { /* page table is full */
 				/* evict according to page replacement */
 				evict_page();
 			} else {
@@ -113,13 +102,20 @@ void SIGSEGV_handler(int sig, siginfo_t *si, void *unused)
 			faults++;
 			page_table[pagenum]->flags = PB | RB;
 
+			/* set read permissions */
 			if (mprotect(virt_page, page_size, PROT_READ) == -1) {
-				fprintf(stderr, "mprotect failed: %s\n", strerror(errno));
+				fprintf(stderr, "mprotect failed: %s\n", 
+						strerror(errno));
 			}
-		} else {
-			page_table[pagenum]->flags = PB | RB | MB;
-			if (mprotect(virt_page, page_size, PROT_READ | PROT_WRITE) == -1) {
-				fprintf(stderr, "mprotect failed: %s\n", strerror(errno));
+		} else {   /* writing to a page for the first time */
+			/* set modify bit */
+			page_table[pagenum]->flags = PB | RB | MB; 
+
+			/* set write permissions */
+			if (mprotect(virt_page, page_size, 
+						PROT_READ | PROT_WRITE) == -1) {
+				fprintf(stderr, "mprotect failed: %s\n", 
+						strerror(errno));
 			}
 		}
 	} else {
@@ -127,7 +123,7 @@ void SIGSEGV_handler(int sig, siginfo_t *si, void *unused)
 	}
 }
 
-entry_t *entry_allocate(int frame)
+entry_t *entry_allocate(int index)
 {
 	entry_t *p;
 
@@ -135,15 +131,14 @@ entry_t *entry_allocate(int frame)
 		fprintf(stderr, "malloc() failed\n");
 	}
 
-	p->frame = frame;
 	p->flags = 0;
 	p->next = NULL;
-	p->index = -1;
+	p->index = index;
 
 	return p;
 }
 
-int get_table_index(void *addr) 
+int get_table_index(const void *addr) 
 {
 	return (int) ((addr - vm_start) / (page_size));
 }
@@ -160,13 +155,13 @@ void history_add(entry_t *t)
 	} else if (type == CLOCK) {
 		history_add_clock(t);
 	} else {
-		fprintf(stderr, "unrecognized page replacement type %d\n", type);
+		fprintf(stderr, "unknown page replacement type %d\n", type);
 	}
 }
 
 void history_add_fifo(entry_t *t)
 {
-	if (page_hist->head == NULL) {
+	if (page_hist->head == NULL) {  /* empty page history */
 		page_hist->head = t;
 		page_hist->tail = t;
 	} else {
@@ -189,15 +184,15 @@ void evict_page(void)
 	} else if (type == CLOCK) {
 		evict_page_clock();
 	} else {
-		fprintf(stderr, "unrecognized page replacement type %d\n", type);
+		fprintf(stderr, "unknown page replacement type %d\n", type);
 	}
 }
 
 void evict_page_fifo(void)
 {
-	entry_t *head;
-	int pagenum;
-	void *virt_page;
+	entry_t *head;    /* current head of the history, the page to evict */
+	int pagenum;      /* page number of virtual page to evict */
+	void *virt_page;  /* address of the virtual page to evict */
 
 	head = page_hist->head;
 	pagenum = head->index;
@@ -210,6 +205,8 @@ void evict_page_fifo(void)
 	head->next = NULL;
 
 	virt_page = get_page_base(pagenum);
+
+	/* unset permissions on the page */
 	if (mprotect(virt_page, page_size, PROT_NONE) == -1) {
 		fprintf(stderr, "mprotect failed: %s\n", strerror(errno));
 	}
--- cmpsc473/project3/473_mm.h
+++ cmpsc473/project3/473_mm.h
@@ -9,31 +9,38 @@
 #define RB    0x2    /* reference bit      */
 #define MB    0x1    /* modify bit         */
 
+/**
+ * struct _entry_t - information about a virtual page, 
+ *                   either in the page table or page history
+ *
+ */
 struct _entry_t {
-	int frame;
-	int flags;
-	int index;
-	struct _entry_t *next;
+	int flags;  /* present, modify and reference bits */
+	int index;  /* index in the page table */
+	struct _entry_t *next;  /* pointer to next page in the history */
 };
 typedef struct _entry_t entry_t;
 
+/**
+ * struct _list_pages - singly-linked list of virtual pages
+ *
+ */
 struct _list_pages {
-	entry_t *head;
-	entry_t *tail;
+	entry_t *head;  /* head of the list, NULL indicates empty list */
+	entry_t *tail;  /* tail of the list */
 };
 typedef struct _list_pages list_pages;
 
 typedef void sighandler_t(int, siginfo_t *, void *);
 
 int type;         /* page replacement policy */
-int faults;
-int write_backs;
-int table_size;
-int offset_bits;
-int max_frames;
-int cur_frames;
-int page_size;
-void *vm_start;
+int faults;       /* number of page faults */
+int write_backs;  /* number of write-backs on modified pages */
+int table_size;   /* number of page table entries */
+int max_frames;   /* number of physical frames available */
+int cur_frames;   /* number of frames currently being used */
+int page_size;    /* size of a page in the system */
+void *vm_start;   /* base address of virtual memory */
 
 /**
  * 'mm_init()' initializes the memory management system. 
@@ -54,7 +61,7 @@ void mm_init(void* vm, int vm_size, int n_frames, int pagesize, int policy);
  *
  * @return total number of page faults
  */
-unsigned long mm_report_npage_faults(); 
+unsigned long mm_report_npage_faults(void); 
 
 /**
  * mm_report_nwrite_backs - return the total number of write backs of 
@@ -62,46 +69,101 @@ unsigned long mm_report_npage_faults();
  *
  * @return total number of write backs
  */
-unsigned long mm_report_nwrite_backs();
+unsigned long mm_report_nwrite_backs(void);
 
 /**
+ * install_signal_handler - install a signal handler for this process
  *
- *
+ * @param sig: signal number to install a signal handler for
+ * @param func: name of the signal handling function to use for this signal
+ * @return int 0 success, -1 error
  */
 int install_signal_handler(int sig, sighandler_t func);
 
 /**
+ * SIGSEGV_handler - signal handler function for SIGSEGV signals
  *
+ * @param sig: SIGSEGV signal number
+ * @param si: signal interrupt information
+ * @param unused: unused parameter, must be included for 
+ *                this form of a signal handler
+ * @return void
  */
 void SIGSEGV_handler(int sig, siginfo_t *si, void *unused);
 
 /**
+ * entry_allocate - allocate a new entry_t
  *
+ * @param index: this virtual page's index in the page table
+ * @return pointer to new entry_t on success, NULL failure
  */
-entry_t *entry_allocate(int frame);
+entry_t *entry_allocate(int index);
 
 /**
+ * get_table_index - get the index in the page table for a memory address
  *
+ * @param addr: address in memory
+ * @return page number for addr
  */
-int get_table_index(void *addr);
+int get_table_index(const void *addr);
 
 /**
+ * get_page_base - get the base address for a virtual page
  *
+ * @param pagenum: index on the page table
+ * @return address of the virtual page at index pagenum in the page table
  */
 void *get_page_base(int pagenum);
 
+/**
+ * history_add - add a page to the history based on the 
+ *               current page replacement scheme
+ *
+ * @param t: virtual page to add to the page history
+ * @return void
+ */
 void history_add(entry_t *t);
-void history_add_fifo(entry_t *t);
-void history_add_clock(entry_t *t);
-
 
 /**
+ * history_add_fifo - add a page to the history based on
+ *                    First-In-First-Out page replacement 
  *
+ * @param t: virtual page to add to the page history
+ * @return void
+ */
+void history_add_fifo(entry_t *t);
+
+/**
+ * history_add_clock - add a page to the history based on
+ *                     clock page replacement
+ *
+ * @param t: virtual page to add to the page history
+ * @return void
+ */
+void history_add_clock(entry_t *t);
+
+/**
+ * evict_page - evict a page from physical memory based on
+ *              the current page replacement scheme
+ *
+ * @return void
  */
 void evict_page(void);
 
+/**
+ * evict_page_fifo - evict a page from physical memory based on
+ *                   First-In-First-Out page replacement
+ *
+ * @return void
+ */
 void evict_page_fifo(void);
 
+/**
+ * evict_page_clock - evict a page from physical memory based on
+ *                    clock page replacement scheme
+ *
+ * @return void
+ */
 void evict_page_clock(void);
 
 #endif
