61ed4708454122e05a34050cd5ee813ffa8f9270
brian.golden09@gmail.com <brian.golden09@gmail.com@44a738fa-cea4-4c8e-a93f-a88c45f461a7>
2013-04-26 19:20:17 +0000

    nway-combination(L,S) - true when S is a combination of the list of lists L
    git-svn-id: https://subversion.assembla.com/svn/bgolden_cmpsc@91 44a738fa-cea4-4c8e-a93f-a88c45f461a7

--- cmpsc461/hw13/hw13.pl
+++ cmpsc461/hw13/hw13.pl
@@ -6,28 +6,34 @@
  */
 
 /**
- * combination - check if a list is a combination of two lists,
- *   i.e. the relative order elements of the two lists is preserved
+ * combination- true when C is a combination of A and B, 
+ *   i.e. C maintains the relative order of both A and B
  */
-
 combination([], [], _).
-combination([], Ys, Ys).
-combination(Xs, [], Xs). 
-combination(Xs, Ys, Zs) :- 
-	append(Xs,Ys,Cs), same_length(Zs, Cs),
-	hasOrder(Xs, Zs), hasOrder(Ys, Zs), 
-	permute(Cs, Zs).
+combination([], B, B).
+combination(A, [], A). 
+combination(A, B, C) :- 
+	append(A, B, Zs), same_length(C, Zs),
+	hasOrder(A, C), hasOrder(B, C), 
+	permutation(Zs, C).
 
 hasOrder([], _).
 hasOrder([X|Xs], [Y|Ys]) :- (X=Y, hasOrder(Xs, Ys)); hasOrder([X|Xs], Ys).
 
+/**
+ * nway-combinatio(L,S) - true when L is a list of lists and 
+ *    S is combination of all those lists 
+ */
+nway-combination([], _).
+nway-combination(L, S) :- 
+	append(L, Zs), same_length(Zs, S),
+	permutation(Zs, S),
+	rnway(L, S).
+
+rnway([],_).
+rnway([X|Xs], S) :- hasOrder(X, S), rnway(Xs, S).
+
 /* aux predicates */
-permute([],[]).
-permute([X|Xs], Zs) :- permute(Xs,Ys), insert(X,Ys,Zs).
-
-insert(X, Xs, [X|Xs]).
-insert(X, [Y|Ys], [Y|Zs]) :- insert(X, Ys, Zs).
-
 upto(0,[]).
 upto(N,[N|Xs]) :- N>0, M is N-1, upto(M,Xs).
 
